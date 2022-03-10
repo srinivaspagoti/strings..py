bda9303aa2c6382d07f3af2a0f93281ed9657a34
p srinivas <100501528+srinivaspagoti@users.noreply.github.com>
2022-03-10 08:04:22 +0530

    Add files via upload

---
+++ strings.py
@@ -0,0 +1,42 @@
+# a="python"
+# a[0]="P"
+
+# print(dir(str))
+
+# x="hELLo pyThon"
+# print(x.capitalize())
+
+# x="hEllOpYThon"
+# print(x.capitalize())
+
+# x="hEllo pyThon"
+# print(x.casefold())
+# print(x.lower())
+
+# x="srinivas naidu"
+# print(len(x))
+# print(x.center)
+# print(x.center())
+# print(x.center(25))
+# print(x.center(25,'/'))
+
+
+#j="srinivas naidu"
+# print(j.replace("naidu","pagoti"))
+# print(j.replace("naidu",str('p')))
+# print(j.replace('i','t',2))
+
+# k="   srinivas naidu   "
+# print(k.strip())
+# print(k.lstrip())
+# print(k.rstrip())
+# print(len(k))
+
+# h="pagoti","srinivas","naidu"
+# print(h,type(h))
+# g="king".join(h)
+# print(g)
+
+f="srinivas naidu"
+print(f.removeprefix("sri"))
+print(f.removesuffix("idu"))
